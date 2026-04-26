3f80e371571e9d70ed98fca20959c284bc3f421c
21999 <nastyakitty2008@mail.ru>
2026-04-26 12:48:05 +0300

    feat!: new methods

--- src/test/java/exam/ApiTest.java
+++ src/test/java/exam/ApiTest.java
@@ -1,4 +1,131 @@
 package exam;
 
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApiTest {
+    static String id = "";
+    static String isbn = "";
+
+    @Test
+    @DisplayName("Книги с фильтрацией")
+    void getBooksWithParams(){
+        Map<String, Integer> queryParams = new HashMap<>();
+        queryParams.put("page", 0);
+        queryParams.put("size", 5);
+        Methods.getParams(queryParams);
+    }
+
+    @Test
+    @DisplayName("Книга по id")
+    void getBookById() {
+        id = "31";
+        Methods.getByID(id);
+    }
+
+    @Test
+    @DisplayName("Книга по isbn")
+    void getBookByIsbn(){
+        isbn = "978-0141182803";
+        Methods.getByIsbn(isbn);
+    }
+
+    @Test
+    @DisplayName("Создать книгу")
+    void addBook(){
+        String newBook = new JSONObject()
+                .put("isbn", "897-1234567890")
+                .put("title", "Магическая битва том.1")
+                .put("author", "Гэгэ Агутами")
+                .put("genre", "сёнэн")
+                .put("year", 2018)
+                .put("price", 1000)
+                .put("stock", 100)
+                .put("pages", 384).toString();
+        Methods.postBook(newBook);
+    }
+
+    @Test
+    @DisplayName("Полное обновление книги")
+    void updateBook(){
+        Map<String, Integer> pathParam = new HashMap<>();
+        pathParam.put("id", 5);
+
+        // JSON для обновления
+        String updates = """
+        {
+            "isbn": "978-0141182803",
+            "title": "Магическая битва том.2",
+            "author": "Гэгэ Акутами",
+            "genre": "сёнэн, боевик",
+            "year": 2025,
+            "price": 1500,
+            "stock": 80,
+            "rating": 4.8,
+            "pages": 392
+        }
+        """;
+        Methods.putBook(pathParam, updates);
+    }
+
+    @Test
+    @DisplayName("Частичное обновление книги")
+    void patchBook() {
+        Map<String, Integer> pathParam = new HashMap<>();
+        pathParam.put("id", 5);
+
+        String partialUpdate = new JSONObject()
+                .put("price", 1200)
+                .put("stock", 50)
+                .toString();
+
+        Methods.patchBook(pathParam, partialUpdate);
+    }
+
+    @Test
+    @DisplayName("Удаление книги по ID")
+    void deleteBookById() {
+        String id = "5";
+
+        Methods.deleteBook(id);
+    }
+
+    @Test
+    @DisplayName("Проверка наличия на складе по ID")
+    void bookInStock(){
+        String id = "5";
+
+        if (Methods.isBookInStock(id)) {
+            System.out.println("В наличии!");
+        }
+    }
+
+    @Test
+    @DisplayName("Добавить отзыв на книгу")
+    void addBookReview() {
+        Map<String, Integer> pathParam = new HashMap<>();
+        pathParam.put("id", 5);  // ID книги
+
+        // Минимальный отзыв (только rating)
+        String review = new JSONObject()
+                .put("rating", 5)
+                .put("comment", "Отличная книга!")
+                .put("reviewerName", "Сатору Годжо")
+                .toString();
+
+        Methods.addBookReview(pathParam, review);
+    }
+
+    @Test
+    @DisplayName("Получить отзывы на книгу")
+    void getBookReviews() {
+        Map<String, Integer> pathParam = new HashMap<>();
+        pathParam.put("id", 31);
+
+        Methods.getBookReviews(pathParam);
+    }
 }
--- src/test/java/exam/Methods.java
+++ src/test/java/exam/Methods.java
@@ -1,4 +1,177 @@
 package exam;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
 public class Methods {
+
+    static String HEADER1 = "X-API-Key";
+    static String HEADER2 = "bookstore-2026-secret";
+
+    static void getParams(Map queryParams){
+        String body = given()
+                .params(queryParams)
+                .when()
+                .log().all()
+                .get("http://10.82.196.214:8085/books")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        System.out.println(body);
+    }
+
+    static void getByID(String id) {
+        System.out.println(id);
+        String body = given()
+                .when()
+                .log().all()
+                .get("http://10.82.196.214:8085/books/" + id)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        System.out.println(body);
+    }
+
+    static void getByIsbn(String isbn) {
+        String body = given()
+                .when()
+                .log().all()
+                .get("http://10.82.196.214:8085/books/isbn/"+isbn)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        System.out.println(body);
+    }
+
+    static void postBook(String book){
+        String body = given()
+                .header(HEADER1, HEADER2)
+                .when()
+                .contentType(ContentType.JSON)
+                .body(book)
+                .log().all()
+                .post("http://10.82.196.214:8085/books")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body()
+                .asString();
+        System.out.println(body);
+    }
+
+    static void putBook(Map pathParams, String updates){
+        String body = given()
+                .pathParams(pathParams)
+                .header("X-API-Key", "bookstore-2026-secret")
+                .contentType(ContentType.JSON)
+                .body(updates)
+                .log().all()
+                .when()
+                .put("http://10.82.196.214:8085/books/{id}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .body().asString();
+        System.out.println(body);
+    }
+
+    static void patchBook(Map<String, Integer> pathParams, String partialUpdates) {
+        String body = given()
+                .pathParams(pathParams)
+                .header(HEADER1, HEADER2)
+                .contentType(ContentType.JSON)
+                .body(partialUpdates)
+                .log().all()
+                .when()
+                .patch("http://10.82.196.214:8085/books/{id}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        System.out.println(body);
+    }
+
+    static void deleteBook(String id) {
+        String body = given()
+                .header(HEADER1, HEADER2)
+                .log().all()
+                .when()
+                .delete("http://10.82.196.214:8085/books/" + id)
+                .then()
+                .log().all()
+                .statusCode(204)
+                .extract().asString();
+        System.out.println("Книга ID=" + id + " удалена");
+    }
+
+    static boolean isBookInStock(String bookId) {
+        Response response = given()
+                .header(HEADER1, HEADER2)
+                .when()
+                .get("http://10.82.196.214:8085/books/" + bookId)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+        int stock = json.getInt("stock");
+
+        System.out.println("Stock для ID " + bookId + ": " + stock);
+        return stock > 0;
+    }
+
+    // Добавьте в класс Methods:
+    static void addBookReview(Map<String, Integer> pathParams, String reviewJson) {
+        String response = given()
+                .pathParams(pathParams)
+                .header(HEADER1, HEADER2)
+                .contentType(ContentType.JSON)
+                .body(reviewJson)
+                .log().all()
+                .when()
+                .post("http://10.82.196.214:8085/books/{id}/reviews")
+                .then()
+                .log().all()
+                .statusCode(201)  // Created
+                .extract()
+                .body()
+                .asString();
+        System.out.println(response);
+    }
+
+    // Добавьте в класс Methods:
+    static void getBookReviews(Map<String, Integer> pathParams) {
+        String response = given()
+                .pathParams(pathParams)
+                .header(HEADER1, HEADER2)
+                .log().all()
+                .when()
+                .get("http://10.82.196.214:8085/books/{id}/reviews")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        System.out.println("Отзывы на книгу: " + response);
+    }
 }
